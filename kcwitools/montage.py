063b41ea337912891ee409aad3e308b3a1514e89
John M. O'Meara <astronomeara@gmail.com>
2018-11-07 09:24:29 -0500

    adding ability to rectify pixels

--- kcwitools/montage.py
+++ kcwitools/montage.py
@@ -62,7 +62,7 @@ def run_montage(infils,outdir='Montage/',outfil="Montage.fits",northup=False,
         tab=Table.read('cubes.hdr',format='ascii',data_end=19)
         tab[17][2]='0.000000000'
         tab.add_row(['END',' ',' ')
-        tab.write('cubes.hdr')
+        tab.write('cubes.hdr',format='ascii')
         
     #second part of montage
     for fil in inputs:
